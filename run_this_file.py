f2ec34f3743df7c5bf70c8f477b0fa1d22c294ac
Lance Negrut <lance@MacBook-Air-755.local>
2025-11-23 18:23:13 -0500

    set_up_database

--- run_this_file.py
+++ run_this_file.py
@@ -1,7 +1,27 @@
 # Lance
 
+
+def set_up_database(db_name):
+    """
+    Sets up a SQLite database connection and cursor.
+
+    Parameters
+    -----------------------
+    db_name: str
+        The name of the SQLite database.
+
+    Returns
+    -----------------------
+    Tuple (Cursor, Connection):
+        A tuple containing the database cursor and connection objects.
+    """
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path + "/" + db_name)
+    cur = conn.cursor()
+    return cur, conn
+
 def main():
-    pass
+    set_up_database("tcg_data")
 
 if __name__ == "__main__":
     main()
