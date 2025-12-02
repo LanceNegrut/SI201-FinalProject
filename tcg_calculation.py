4d4358484c0dd7d057fa732a7b4afb9b7e3091fd
breyesmx <breyes@umich.edu>
2025-12-02 09:51:23 -0500

    Update: part two of the write calculation into a txt file

--- tcg_calculation.py
+++ tcg_calculation.py
@@ -218,6 +218,22 @@ def write_calculation_to_file(pokemon_total_cards, pokemon_sets, yugioh_total_ca
         all_years = set(pokemon_total_cards.keys()) | set(pokemon_sets.keys()) | set(yugioh_total_cards.keys()) | set(yugioh_sets.keys()):
         sorted_years = sorted(list(all_years))
 
+        f.write("Year | Pokemon Cards Released | Pokemon Sets Released | Yu-Gi-Oh Cards Released | Yu-Gi-Oh Sets Released |")
+        f.write('\n')
+        f.write("-" * 80 + '\n')
+        f.write('\n')
+
+        # columns width for formatting 
+
+        column_widths = 15
+        year_widths = 6 
+
+        # header 
+
+        header = f'{"Year":<{year_widths}} | {"Pokemon Cards Released":<{column_widths}} | {"Pokemon Sets Released":<{column_widths}} | {"Yu-Gi-Oh Cards Released":<{column_widths}} | {"Yu-Gi-Oh Sets Released":<{column_widths}} |'
+
+       
+
 | 
 
 
