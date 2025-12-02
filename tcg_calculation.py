4b7230536d02e48e18759aee1bc0e5bb16745500
breyesmx <breyes@umich.edu>
2025-12-02 16:29:39 -0500

    Update: copy final_calcualtion.py to tcg_calculation.py

--- tcg_calculation.py
+++ tcg_calculation.py
@@ -195,6 +195,33 @@ def create_yugioh_histogram(data, title, xlabel, ylabel):
     plt.tight_layout()
     plt.show()
 
+
+def joining_tables(conn):
+
+    """
+    Join tables to combine data from Pokemon, Yu-Gi-Oh cards per year. 
+    
+    """
+
+    pokemon_data = calculate_pokemon_total_per_year(conn)
+    yugioh_data = calculate_yugioh_total_per_year(conn)
+
+    all_years = set(pokemon_data.keys()) | set(yugioh_data.keys())
+    combined_data = {}
+
+    for year in all_years:
+        combined_data[year] = pokemon_data.get(year, 0) + yugioh_data.get(year, 0)
+    return combined_data, pokemon_data, yugioh_data
+
+def create_histogram(pokemon_data, yugioh_data, combined_data):
+    """
+
+    Create histogram comparing Pokemon, Yu-Gi-Oh, and combined card release per year and showing the trend and differences.
+    
+    """
+
+    all_years = sorted
+
 def write_calculation_to_file(pokemon_total_cards, pokemon_sets, yugioh_total_cards, yugioh_sets, filename='All_calculation.txt'):
     # Rubric write calculation result to a text file:
 
